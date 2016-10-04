e74ad2a45fc048283445904a0a22f5e1f05f1de3
Guillem Barba <guillem@nan-tic.com>
2016-10-04 16:29:12 +0200

    Adapt tests to 4.0 and data model changes

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,8 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from .test_analytic_purchase_warehouse import suite
+try:
+    from trytond.modules.analytic_purchase_warehouse.tests.test_analytic_purchase_warehouse import suite
+except ImportError:
+    from .test_analytic_purchase_warehouse import suite
+
+__all__ = ['suite']
